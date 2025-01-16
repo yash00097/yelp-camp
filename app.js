af713f6ef227b1725b9cf40adac5d9e45605a511
yash00097 <yashwanthvuppala77@gmail.com>
2025-01-16 17:46:46 +0530

    adding basic styles

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import { connectDB } from './db/connectDB.js';
 import Campground from './models/campground.model.js';
 import methodOverride from 'method-override';
+import ejsMate from 'ejs-mate';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,6 +13,7 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
+app.engine("ejs", ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.set("view engine", "ejs");
--- seeds/index.js
+++ seeds/index.js
@@ -11,9 +11,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i=0; i<50; i++){
         const rand1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20) + 10;
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[rand1000].city}, ${cities[rand1000].state}`
+            location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, quos. Quamek",
+            price: price
         })
         await camp.save();
     }
